9c8941fa28abdbe2353d526780db368c665b945d
Mason Suess <masonsuess@gmail.com>
2025-01-24 18:40:14 -0500

    Particles+grvity

--- Fluid.cs
+++ Fluid.cs
@@ -3,26 +3,43 @@ using System;
 using System.Runtime.InteropServices;
 
 
-[StructLayout(LayoutKind.Sequential)]
-[StructLayout(LayoutKind.Sequential, Size=44)]
-public struct Particle {
-	public float density;
-	public float pressure;
-	public Vector3 currentForce;
-	public Vector3 velocity;
-	public Vector3 position;
-}
 
-
-public partial class Fluid : Node
-{
+public partial class Fluid : Node2D
+{	
+	public float gravity;
+	Vector2 Pposition;
+	Vector2 Pvelocity;
+	
+	public static class Predef
+	{
+		public static readonly Color blue = new Color(0.65f, 0.85f, 0.173f);
+	}
+	
+	
+	public override void _Draw()
+	{
+		DrawCircle(Pposition, 5, Predef.blue);
+		
+	
+	}
+	
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		
+		Position = GetViewportRect().Size / 2;
+		
 	}
+	
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Pvelocity.Y += gravity * (float)delta;
+		Pposition += Pvelocity * (float)delta;
+		QueueRedraw();
 	}
+	
+	
 }
---
+++ Simulation.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class Simulation : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+		//Prepare to spawn particles
+		
+		//Prepare data buffers
+		
+		//display
+		
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+		//run simulation
+	}
+}
